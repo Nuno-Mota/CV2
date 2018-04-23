bb1036e1683b99019ee18410f3431bf2beed8253
Nuno Mota <nunonetomota@gmail.com>
2018-04-23 21:22:32 +0200

    very small changes

--- Assignment 1/Code/getExampleData.m
+++ Assignment 1/Code/getExampleData.m
@@ -1,21 +1,17 @@
 function [source_data, target_data] = getExampleData(path_to_source, path_to_target)
 
-source = load(path_to_source);
-clear path_to_source;
+source = load(path_to_source); clear path_to_source;
 source = source.source';
 
-target = load(path_to_target);
-clear path_to_target;
+target = load(path_to_target); clear path_to_target;
 target = target.target';
 
 source_data.x = source(:, 1);
 source_data.y = source(:, 2);
-source_data.z = source(:, 3);
-clear source;
+source_data.z = source(:, 3); clear source;
 
 target_data.x = target(:, 1);
 target_data.y = target(:, 2);
-target_data.z = target(:, 3);
-clear target;
+target_data.z = target(:, 3); clear target;
 end
 
