03f482694cd83439d0c838a1944e4323ba7f65ee
mrivar <martin7557@gmail.com>
2018-04-24 18:15:53 +0200

    Add preliminar code for real IPC

--- Assignment 1/Code/main.m
+++ Assignment 1/Code/main.m
@@ -1,12 +1,15 @@
 clc
 
-path_to_data_source = "Data/source.mat";
-path_to_data_target = "Data/target.mat";
+path_to_data_source = sprintf("Data/data/%010d.pcd",1);
+source = readPcd(path_to_data_source); clear path_to_data_source;
 
-source = load(path_to_data_source); clear path_to_source;
-source = source.source';
+num_images = 99;
+for i=2:num_images
+    path_to_data_target = sprintf("Data/data/%010d.pcd",i);
 
-target = load(path_to_data_target); clear path_to_target;
-target = target.target';
+    target = readPcd(path_to_data_target);
 
-[R, t] = ICP(source, target);
+    [R, t] = ICP(source, target, false);
+
+    source = target;
+end
