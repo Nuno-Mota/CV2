c87cab358e7c4c76ddc22f64a9fad6e6ce8e6f13
mrivar <martin7557@gmail.com>
2018-04-24 16:55:21 +0200

    Move Sample data ICP to DemoICP.m

--- Assignment 1/Code/DemoICP.m
+++ Assignment 1/Code/DemoICP.m
@@ -5,7 +5,11 @@ path_to_data_target = "Data/target.mat";
 
 [source_data, target_data, plot_data] = getExampleData(path_to_data_source, path_to_data_target);
 
+figure('Name', 'Source vs. Target')
 fscatter3(plot_data)
 xlabel('X')
 ylabel('Y')
 zlabel('Z')
+
+figure('Name', 'Transformed Source vs. Target')
+[R, t] = ICP(source, target);
