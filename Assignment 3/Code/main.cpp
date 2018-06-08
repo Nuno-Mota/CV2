2349c4f3a5e20e2d4ed83ac1ad9244592cdfd567
Nuno-Mota <nunonetomota@gmail.com>
2018-06-08 05:24:17 +0200

    Finished code

--- Assignment 3/Code/main.cpp
+++ Assignment 3/Code/main.cpp
@@ -38,12 +38,20 @@
  * ********* CONSTANTS DEFINITION ********* *
  * **************************************** *
  ********************************************/
-// TODO: Pick good values (how?)
-const float MAX_DEPTH = 1; // 
 
-const int POISSON_DEPTH = 12;
-const float POISSON_SCALE = 1.75;
-const int   POISSON_SAMPLES_PER_NODE = 20;
+const float MAX_DEPTH = 1.0; 
+
+const int   POISSON_DEPTH = 7;
+const float POISSON_SCALE = 1.5;
+const int   POISSON_SAMPLES_PER_NODE = 10;
+
+const int   MC_GRID_RESOLUTION = 25;
+
+const float OCTREE_RESOLUTION = 0.05f;
+const float FOCAL_LENGTH_MULTIPLIER = 3.5f;
+
+// Create viewer
+pcl::visualization::PCLVisualizer viewer("Simple Cloud Viewer");
 
 
 
@@ -105,6 +113,50 @@ void computeNormals(pcl::PointCloud<pcl::PointNormal>::Ptr cloud) {
 }
 
 
+// Change camera function
+void keyboardEventOccurred(const pcl::visualization::KeyboardEvent& event, void* nothing) {
+    if (event.getKeySym() == "f" && event.keyDown()) {
+        viewer.setCameraPosition(0.1, 0.1, -1.8, 0.2, 0.2, 0.2, 0, -1, 0);
+    }
+    else if (event.getKeySym() == "b" && event.keyDown()) {
+        viewer.setCameraPosition(0.1, 0.1, 2.2, 0.2, 0.2, 0.2, 0, -1, 0);
+    }
+    else if (event.getKeySym() == "r" && event.keyDown()) {
+        viewer.setCameraPosition(-1.8, 0.1, 0.1, 0.2, 0.2, 0.2, 0, -1, 0);
+    }
+    else if (event.getKeySym() == "l" && event.keyDown()) {
+        viewer.setCameraPosition(2.2, 0.1, 0.1, 0.2, 0.2, 0.2, 0, -1, 0);
+    }
+    else if (event.getKeySym() == "t" && event.keyDown()) {
+        viewer.setCameraPosition(0.1, -1.8, 0.1, 0.2, 0.2, 0.2, 0, -1, 0);
+    }
+}
+
+
+// Helper function used to visualize the mesh
+void visualise (pcl::PolygonMesh &triangles) {
+
+    // Add mesh
+    viewer.addPolygonMesh(triangles, "meshes", 0);
+
+    // Define viewer parameters
+    viewer.setBackgroundColor(1, 1, 1);
+    viewer.addCoordinateSystem(1.0);
+    viewer.initCameraParameters ();
+
+    // Set initial camera position
+    viewer.setCameraPosition(0.1, 0.1, -1.2, 0.2, 0.2, 0.2, 0, -1, 0);
+
+    viewer.registerKeyboardCallback(&keyboardEventOccurred, (void*) NULL);
+
+    // Keep viewer open
+    while (!viewer.wasStopped()){
+        viewer.spinOnce(100);
+        boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+    }
+}
+
+
 
 
 
@@ -128,7 +180,7 @@ pcl::PointCloud<pcl::PointNormal>::Ptr mergingPointClouds(Frame3D frames[]) {
 
         // Get curent frame's point cloud (pointer).
         pcl::PointCloud<pcl::PointNormal>::Ptr current_pc = getPointCloud(depthImage, focalLength, MAX_DEPTH);
-
+        
         // Compute curent frame's point cloud's normals.
         computeNormals(current_pc);
 
@@ -163,8 +215,7 @@ pcl::PolygonMesh createMesh(pcl::PointCloud<pcl::PointNormal>::Ptr pointCloud, C
             // Set input to the Poisson method.
             poisson.setInputCloud(pointCloud);
 
-            // Define Poisson's parameters. (other parameters seemed a bit complicated to exactly
-            // understand what their purpose was, and were assumed to be out of the scope of the project).
+            // Define Poisson's parameters.
             poisson.setDepth(POISSON_DEPTH);
             poisson.setScale(POISSON_SCALE);
             poisson.setSamplesPerNode(POISSON_SAMPLES_PER_NODE);
@@ -174,17 +225,13 @@ pcl::PolygonMesh createMesh(pcl::PointCloud<pcl::PointNormal>::Ptr pointCloud, C
             break;
         }
         case MarchingCubes: {
-            // How to choose between "Hoppe" and "RBF"?
             pcl::MarchingCubesHoppe<pcl::PointNormal> marchingCubes;
-            //pcl::MarchingCubesRBF<pcl::PointXYZRGBNormal> marchingCubes;
 
             // Set input to the Marching Cubes method.
             marchingCubes.setInputCloud(pointCloud);
 
             // Define Marching Cube's parameters.
-            //marchingCubes.setDistanceIgnore(20.0f); // Cannot find this function, for some weird reason.
-            marchingCubes.setGridResolution (30, 30, 30);
-            //marchingCubes.setIsoLevel (0.5f);
+            marchingCubes.setGridResolution(MC_GRID_RESOLUTION, MC_GRID_RESOLUTION, MC_GRID_RESOLUTION);
 
             // Set Marching Cube's search method.
             pcl::search::KdTree<pcl::PointNormal>::Ptr search_tree (new pcl::search::KdTree<pcl::PointNormal>);
@@ -197,6 +244,122 @@ pcl::PolygonMesh createMesh(pcl::PointCloud<pcl::PointNormal>::Ptr pointCloud, C
         }
     }
     return triangles;
+}
+
+
+// Helper struct, used in computing vertices' colour.
+struct point {
+    bool empty = true;
+    std::vector<uint8_t> r;
+    std::vector<uint8_t> g;
+    std::vector<uint8_t> b;
+} ;
+
+
+// Function used to determine vertice colour, given colour observed in each frame.
+uint8_t averageOf3Closest(std::vector<uint8_t> colour_channel) {
+    std::vector<uint8_t> best(3);
+    int smallest_dist = 1000; // Anything bigger than the max possible value for the sum of distances between 3 [0-255] values
+
+    // This next bit can definitely be made faster, but not in the mood for it now.
+    // Also, there is no guarantee whatsoever that the point will have colour for at least 3 frames, so this would break
+    // in those situations. However, that doesn't seem to be the case with these specific images, thus we didn't code for it.
+    for (uint8_t value1 : colour_channel) {
+        for (uint8_t value2 : colour_channel) {
+            for (uint8_t value3 : colour_channel) {
+                if (&value1 != &value2  && &value1 != &value3 && &value2 != &value3) {
+                    int current_val = abs(value1-value2) + abs(value1-value3) + abs(value2-value3);
+                    if (current_val < smallest_dist) {
+                        smallest_dist = current_val;
+                        best[0] = value1;
+                        best[1] = value2;
+                        best[2] = value3;
+                    }
+                }
+            }
+        }
+    }
+
+    return std::accumulate(best.begin(), best.end(), 0.0)/(uint8_t)best.size();
+}
+
+
+// Function that adds texture to an existing mesh.
+pcl::PolygonMesh addTextureFromMesh(pcl::PolygonMesh &mesh, Frame3D frames[]) {
+
+    pcl::TextureMapping<pcl::PointXYZ> map;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr meshPointCloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(mesh.cloud, *meshPointCloud);
+
+    std::vector<point> cloudPointColours(meshPointCloud->size());
+
+    for (int i = 0; i < 8; i++) {
+        std::cout << boost::format("Texturing from frame %d") % i << std::endl;
+
+        Frame3D frame = frames[i];
+
+        // Get camera parameters
+        pcl::TextureMapping<pcl::PointXYZ>::Camera camera;
+        camera.focal_length = frame.focal_length_*FOCAL_LENGTH_MULTIPLIER; //For some reason works better with this multiplier
+        camera.pose = frame.getEigenTransform();
+        camera.width = frame.rgb_image_.cols;
+        camera.height = frame.rgb_image_.rows;
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr tempPointCloud(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::copyPointCloud(*meshPointCloud, *tempPointCloud);
+
+        // Compute transformed point cloud.
+        pcl::transformPointCloud(*tempPointCloud, *tempPointCloud, camera.pose.inverse());
+
+        // Octree for occlusion determination
+        pcl::TextureMapping<pcl::PointXYZ>::Octree::Ptr octree(new pcl::TextureMapping<pcl::PointXYZ>::Octree(OCTREE_RESOLUTION));
+        octree->setInputCloud(tempPointCloud);
+        octree->addPointsFromInputCloud();
+
+        for (auto polygon : mesh.polygons) {
+            for (auto vertice : polygon.vertices) {
+
+                pcl::PointXYZ temp_point;
+                temp_point.x = tempPointCloud->points.at(vertice).x;
+                temp_point.y = tempPointCloud->points.at(vertice).y;
+                temp_point.z = tempPointCloud->points.at(vertice).z;
+
+                // Determine if point is occluded
+                if (!map.isPointOccluded(temp_point, octree)) {
+
+                    // Determine if point is visible in RGB image
+                    Eigen::Vector2f coordinates;
+                    if (map.getPointUVCoordinates(temp_point, camera, coordinates)) {
+                        int x = (int)(coordinates[0] * camera.width);
+                        int y = (int)(camera.height - (coordinates[1] * camera.height));
+
+                        // Pushback vertice's rgb, for current frame, to vector<struct> of observed RGBs.
+                        cv::Vec3b bgrPixel = frame.rgb_image_.at<cv::Vec3b>(y, x);
+                        cloudPointColours[vertice].r.push_back((uint8_t)bgrPixel[2]);
+                        cloudPointColours[vertice].g.push_back((uint8_t)bgrPixel[1]);
+                        cloudPointColours[vertice].b.push_back((uint8_t)bgrPixel[0]);
+                    }
+                }
+            }
+        }
+    }
+
+    // Compute each vertice's colour
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr colouredPointCloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::fromPCLPointCloud2(mesh.cloud, *colouredPointCloud);
+
+    for (auto polygon : mesh.polygons) {
+            for (auto vertice : polygon.vertices) {
+                colouredPointCloud->points.at(vertice).r = averageOf3Closest(cloudPointColours[vertice].r);
+                colouredPointCloud->points.at(vertice).g = averageOf3Closest(cloudPointColours[vertice].g);
+                colouredPointCloud->points.at(vertice).b = averageOf3Closest(cloudPointColours[vertice].b);
+            }
+    }
+
+    pcl::toPCLPointCloud2(*colouredPointCloud, mesh.cloud);
+
+    return mesh;
 }
 
 
@@ -218,6 +381,11 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    std::cout << "Visualizer opens immediately, but only shows anything at the end." << std::endl;
+    std::cout << "Once it starts showing, key presses can be used to change the camera:" << std::endl;
+    std::cout << "'f' - front, 'b' - back, 'r' - right, 'l' - left, 't' - top" << std::endl;
+    std::cout << "Note: Leaving the mouse pointer within the visualizer window might cause weird behaviour." << std::endl << std::endl;
+
     // Determine the specified reconstruction method
     const CreateMeshMethod reconMode = static_cast<CreateMeshMethod>(std::stoi(argv[2]));
 
@@ -230,56 +398,27 @@ int main(int argc, char *argv[]) {
     pcl::PointCloud<pcl::PointNormal>::Ptr pointCloud;
     pcl::PolygonMesh triangles;
 
+
+    // SECTION 3 & 4 (part common to both): 3D Meshing & Watertighting
+
     // Create one point cloud by merging all frames.
     pointCloud = mergingPointClouds(frames);
 
     // Create a mesh from the cloud, using a reconstruction method,
     // Poisson Surface or Marching Cubes
     triangles = createMesh(pointCloud, reconMode);
-    // TODO: Add normals to mesh!
 
-    // SECTION 3: 3D Meshing & Watertighting --> finishes here. Nothing more required.
+    // SECTION 3 --> finishes here. Nothing more required.
 
     // SECTION 4: Coloring 3D Model
-    if (argv[3][0] == 't') {}//addTextureFromMesh(triangles, frames);}
-
-
+    if (argv[3][0] == 't')
+        triangles = addTextureFromMesh(triangles, frames);
 
 
     /***************VISUALIZATION**************/
 
     std::cout << "Finished texturing" << std::endl;
-
-    // Create viewer
-    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
-
-    /* if (argv[3][0] == 'a') { // TODO: change to correct letter, 't'
-        // SECTION 4: Coloring 3D Model
-        // Add colored point cloud to viewer, because it does not support colored meshes
-        pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBNormal> rgb(pointCloud);
-        viewer->addPointCloud<pcl::PointXYZRGBNormal>(pointCloud, rgb, "cloud");
-    } */
-
-    // Add mesh
-    viewer->setBackgroundColor(1, 1, 1);
-
-    // Configure viewer
-    vtkSmartPointer<vtkPolyData> poly_data;
-    pcl::VTKUtils::mesh2vtk(triangles, poly_data);
-    viewer->addModelFromPolyData(poly_data, "poly_data", 0);
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_SHADING,
-                                        pcl::visualization::PCL_VISUALIZER_SHADING_PHONG, "poly_data");
-    viewer->addCoordinateSystem(1.0);
-    viewer->initCameraParameters();
-
-    // Set initial camera position
-    viewer->setCameraPosition(0.1, 0.1, -1.2, 0.2, 0.2, 0.2, 0, -1, 0);
-
-    // Keep viewer open
-    while (!viewer->wasStopped()) {
-        viewer->spinOnce(100);
-        boost::this_thread::sleep(boost::posix_time::microseconds(100000));
-    }
+    visualise(triangles);
 
     return 0;
 }
